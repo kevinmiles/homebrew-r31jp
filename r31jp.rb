7b154751ddf2cf6616ab9084e11a6b399f516126
David Yamnitsky <davidy@me.com>
2017-02-11 18:43:10 -0500

    Update r31jp.rb

--- r31jp.rb
+++ r31jp.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class R31jp < Formula
     url 'https://github.com/nitsky/r31jp/archive/0.1.tar.gz'
-    sha256 '9eae96a0b8acfa62bc4d0a603eb2ec8bfe5161be91f90234ebc73dff601592ae'
+    sha256 '9472dff18038b0295054e8dff3b9a1750347d9bb074e995d4d3d767e315e9773'
     homepage 'https://github.com/nitsky/r31jp'
     depends_on 'nitsky/r31jp/as31'
     def install
