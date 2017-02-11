5d535b8c10a3b1d9a65c99446d63c83599048f17
David Yamnitsky <davidy@me.com>
2017-02-11 18:43:00 -0500

    Update as31.rb

--- as31.rb
+++ as31.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class As31 < Formula
     url 'https://github.com/nitsky/as31/archive/0.1.tar.gz'
-    sha256 '9eae96a0b8acfa62bc4d0a603eb2ec8bfe5161be91f90234ebc73dff601592ae'
+    sha256 '9472dff18038b0295054e8dff3b9a1750347d9bb074e995d4d3d767e315e9773'
     homepage 'https://github.com/nitsky/as31'
     def install
         system "make"
