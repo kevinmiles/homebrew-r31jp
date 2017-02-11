b662c607042fb10ea21cb79b5afdabc05a4bcc88
David Yamnitsky <davidy@me.com>
2017-02-11 18:41:39 -0500

    Update from SHA1 to SHA256

--- r31jp.rb
+++ r31jp.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class R31jp < Formula
     url 'https://github.com/nitsky/r31jp/archive/0.1.tar.gz'
-    sha1 '2a593c31eca52546689f99a4ed4ef40b081e0cf0'
+    sha256 '9eae96a0b8acfa62bc4d0a603eb2ec8bfe5161be91f90234ebc73dff601592ae'
     homepage 'https://github.com/nitsky/r31jp'
     depends_on 'nitsky/r31jp/as31'
     def install
